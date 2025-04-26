8ab34988c7af63196577d66a2884e95f0a82dda1
DennisGabriel <dennisgabr745@gmail.com>
2025-04-26 15:49:12 -0300

    while example

---
+++ semana_1/while.php
@@ -0,0 +1,20 @@
+<?php
+
+$i = 1;
+
+// first syntax
+while ($i <= 10) {
+  echo $i++;
+  echo "<br>";
+}
+
+
+echo "<br>";
+$i = 1;
+
+while ($i <= 10):
+  echo $i++;
+  echo "<br>";
+endwhile;
+?>
+
