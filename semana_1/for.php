135f31e94308febe663e1986fe516e5c738b8518
DennisGabriel <dennisgabr745@gmail.com>
2025-04-26 16:15:08 -0300

    for examples

---
+++ semana_1/for.php
@@ -0,0 +1,34 @@
+<?php
+
+for($i = 1; $i <= 10; $i++){
+  echo $i . ' ';
+}
+
+echo "<br>";
+// ; ; "empty" expression is true as default in PHP.
+for($i = 1; ;$i++){
+  if($i == 11){
+    break;
+  }
+  echo $i . ' ';
+}
+
+
+echo "<br>";
+
+// Looks like while interaction
+$i = 1;
+for( ; ; ){
+  if($i == 11){
+    break;
+  }
+
+  echo $i . ' ';
+  $i++;
+}
+
+
+echo "<br>"
+// present on documentation, but accuses syntax error.
+//for ($i = 1, $j = 0; $i <= 10; $j += $i, print $i, $i++);
+?>
