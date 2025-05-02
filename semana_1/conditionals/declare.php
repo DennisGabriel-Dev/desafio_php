682699b84d9bcf78baabaf999c4aed37b0a701a7
DennisGabriel <dennisgabr745@gmail.com>
2025-05-01 21:29:48 -0300

    declare php

---
+++ semana_1/conditionals/declare.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(ticks=1);
+
+function tick_handler(){
+  echo "<br>tick_handler() called. \n";
+}
+
+register_tick_function('tick_handler');
+
+$a = 1; // call a tick event
+
+if($a > 0){
+  $a += 1; // call a tick event
+  echo $a++; // call a tick event(twice)
+}
